1bc926e76dde603c51c33f3980bbe5357e3a937e
RulH19 <nurulh298hd@gmail.com>
2023-04-30 15:26:57 +0700

    Tambah Motion Di main setengah

--- app/src/main/java/com/example/appstory/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/appstory/ui/login/LoginActivity.kt
@@ -3,10 +3,33 @@ package com.example.appstory.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appstory.R
+import com.example.appstory.databinding.ActivityLoginBinding
+import com.example.appstory.ui.custom.EmailEditText
+import com.example.appstory.ui.custom.MyButtonLogin
+import com.example.appstory.ui.custom.NameEditText
+import com.example.appstory.ui.custom.PasswordEditText
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var myButtonLogin: MyButtonLogin
+    private lateinit var emailEditText: EmailEditText
+    private lateinit var passwordEditText: PasswordEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        myButtonLogin = binding.loginButton
+        emailEditText = binding.emailEditText
+        passwordEditText = binding.passwordEditText
+
+        setMyButtonEnable()
+    }
+    private fun setMyButtonEnable() {
+        val result1 = emailEditText.text
+        val result2 = passwordEditText
+        myButtonLogin.isEnabled = result1 != null && result1.toString().isNotEmpty() &&
+                result2 != null && result2.toString().isNotEmpty()
     }
 }
--- app/src/main/java/com/example/appstory/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/appstory/ui/register/RegisterActivity.kt
@@ -2,11 +2,36 @@ package com.example.appstory.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.appstory.R
+import com.example.appstory.databinding.ActivityRegisterBinding
+import com.example.appstory.ui.custom.EmailEditText
+
+import com.example.appstory.ui.custom.MyButtonRegister
+import com.example.appstory.ui.custom.NameEditText
+import com.example.appstory.ui.custom.PasswordEditText
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    private lateinit var myButtonRegister: MyButtonRegister
+    private lateinit var nameEditText: NameEditText
+    private lateinit var emailEditText: EmailEditText
+    private lateinit var passwordEditText: PasswordEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        myButtonRegister = binding.registerButton
+        nameEditText = binding.nameEditText
+        emailEditText = binding.emailEditText
+        passwordEditText = binding.passwordEditText
+        setMyButtonEnable()
+    }
+    private fun setMyButtonEnable() {
+        val name = nameEditText.text
+        val email = emailEditText.text
+        val password = passwordEditText
+        myButtonRegister.isEnabled = email != null && email.toString().isNotEmpty() &&
+                password != null && password.toString().isNotEmpty() &&
+                name != null && name.toString().isNotEmpty()
     }
 }
